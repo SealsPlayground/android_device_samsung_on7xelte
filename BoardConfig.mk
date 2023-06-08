fc3ca5cbfbb3cfbd3c89397a103ae1aeb3d459f6
ivanmeler <i_ivan@windowslive.com>
2023-06-08 21:18:52 +0000

    add dummy selinux

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,7 +224,8 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/vendor/bin/hw/rild=27
 
 # SELinux
-#BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS := device/samsung/on7xelte/sepolicy
+BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 
 # Shim
 TARGET_LD_SHIM_LIBS += \
